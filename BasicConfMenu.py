65826e7ff22b0b22df67a1655ebbc16473271e88
CAMERON BIRNIE <1805305@rgu.ac.uk>
2020-04-25 23:04:50 +0100

    Added BasicConfDomain and BasicConfReload functions to interact with the network device.

--- BasicConfMenu.py
+++ BasicConfMenu.py
@@ -52,20 +52,76 @@ class BasicConfHostname(Screen):
 
 
         hostname_command = ["hostname " + hostname]
+
         net_connect = ConnectHandler(**device) 
+
         net_connect.send_config_set(hostname_command)
 
-
- 
         output = net_connect.find_prompt()
+
         print(output)
 
 
-class BasicConfDomain(Screen):        
-    pass
+class BasicConfDomain(Screen):  
+    
+    def BasicConfDomainExecute(self):
 
-class BasicConfReload(Screen):        
-    pass
+        ip_address = self.ids._Basic_Conf_Domain_Layout_.ids.IPv4AddressTextInput.text
+        domain = self.ids._Basic_Conf_Domain_Layout_.ids.DomainTextInput.text
+
+        device = { 
+          'device_type': 'cisco_ios', 
+          'ip': ip_address, 
+          'username': 'Test', 
+          'password': 'cisco123', 
+          } 
+
+
+        config_commands = ["ip domain-name " + domain]
+
+        net_connect = ConnectHandler(**device) 
+
+        output = net_connect.send_config_set(config_commands)
+
+        print(output)
+
+class BasicConfReload(Screen): 
+    
+    def BasicConfReloadExecute(self):
+
+        ip_address = self.ids._Basic_Conf_Reload_Layout_.ids.IPv4AddressTextInput.text
+
+        device = { 
+          'device_type': 'cisco_ios', 
+          'ip': ip_address, 
+          'username': 'Test', 
+          'password': 'cisco123', 
+          } 
+
+
+        net_connect = ConnectHandler(**device) 
+
+        output = net_connect.send_command_timing('reload')
+        if 'Proceed with reload' in output:
+            output += net_connect.send_command_timing('\n')
+            print(output)
+        if 'System configuration has been modified' in output:
+            output += net_connect.send_command_timing('yes')
+            output += net_connect.send_command_timing('\n')
+            print(output)
+        else:
+            # probably should raise an error here (as this is not expected)
+            print('Failed')
+
+
+
+
+        #output = net_connect.find_prompt()
+        #print(output)
+        #net_connect.send_command('do reload', expect_string='confirm')
+        #net_connect.send_command('\n')
+        #print('Reloading Device')
+
 
     
         
