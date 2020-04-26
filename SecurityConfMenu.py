74444999a02f20443c66911b0ea72cc3e6d9bf92
CAMERON BIRNIE <1805305@rgu.ac.uk>
2020-04-26 23:14:25 +0100

    Updated SecurityConfLocalUsernameDatabase screen within SecurityConfMenu.kv to allow user to add a user account to a network device.
    Created SecurityConfLocalUsernameDatabaseExecute function within SecuirtyConfMenu.py to gather variables set by the user and configure the network device

--- SecurityConfMenu.py
+++ SecurityConfMenu.py
@@ -32,7 +32,41 @@ class SecurityConfMenuButtons(BoxLayout):
 
 
 class SecurityConfLocalUsernameDatabase(Screen):        
-    pass
+    
+    def SecurityConfLocalUsernameDatabaseExecute(self):
+
+
+
+        privilege_level = self.ids._Security_Conf_Local_Username_Database_Layout_.ids.SecurityConfLocalUsernameDatabasePrivilegeLayout.ids.PrivilegeLevelSpinner.text
+        new_username = self.ids._Security_Conf_Local_Username_Database_Layout_.ids.SecurityConfLocalUsernameDatabaseUserAndPassLayout.ids.UsernameTextInput.text
+        new_password = self.ids._Security_Conf_Local_Username_Database_Layout_.ids.SecurityConfLocalUsernameDatabaseUserAndPassLayout.ids.PasswordTextInput.text
+
+        device_ip_address = self.ids._IPv4_Target_Device_Layout_.ids.IPv4AddressTextInput.text
+
+        device = { 
+          'device_type': 'cisco_ios', 
+          'ip': device_ip_address, 
+          'username': 'Test', 
+          'password': 'cisco123', 
+          } 
+
+        priv_check = self.ids._Security_Conf_Local_Username_Database_Layout_.ids.SecurityConfLocalUsernameDatabasePrivilegeLayout.ids.PrivilegeLevelSpinner.text
+        secret_check = self.ids._Security_Conf_Local_Username_Database_Layout_.ids.SecurityConfLocalUsernameDatabaseSecretLayout.ids.SecretPasswordTrue.active
+
+        if secret_check == True and priv_check != 'No Privilege Required': #Priv and Secret
+            config_commands = ["username " + new_username + " privilege " + privilege_level + " secret " + new_password]
+        elif  priv_check != 'No Privilege Required': #Priv and password
+            config_commands = ["username " + new_username + " privilege " + privilege_level + " password " + new_password]
+        elif  secret_check == True:  #Secret       
+            config_commands = ["username " + new_username + " secret " + new_password]
+        else:   
+            config_commands = ["username " + new_username + " password " + new_password] #Standard
+
+        net_connect = ConnectHandler(**device) 
+
+        output = net_connect.send_config_set(config_commands)
+
+        print(output)
     
 
 class SecurityConfPasswordEncryption(Screen):        
