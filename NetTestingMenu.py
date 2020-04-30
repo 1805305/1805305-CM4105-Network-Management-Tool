b04c9cc45ad0e8275f01d746d53c80cc3e1986d4
CAMERON BIRNIE <1805305@rgu.ac.uk>
2020-04-30 02:26:25 +0100

    Updated and defined placeholder Ping Menu within NetTestingMenu.kv to allow user to enter IP address to be pinged and the amount of retries. The result will then be displayed in the middle of the screen.
    Created NetTestingPingExecute function within NetTestingMenu.py to provide functionality to ping network device and display the results appropriatley.
    
    Used Module MultiPing by Juergen Brendel to facilitate the ping command

--- NetTestingMenu.py
+++ NetTestingMenu.py
@@ -17,6 +17,11 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import Screen
 
+from kivy.properties import ListProperty, StringProperty
+
+from multiping import MultiPing
+from multiping import multi_ping
+
 class NetTestingMenuButtons(BoxLayout):
 
     def NetTestingPingButton(self, instance):
@@ -24,9 +29,45 @@ class NetTestingMenuButtons(BoxLayout):
 
 
 class NetTestingPing(Screen):        
-    pass
-
-
     
+    device_ip_address = ListProperty([''])
+    result_of_ping = StringProperty('Sucess')
+
+    def NetTestingPingExecute(self):
+
+        #Need to add a method of ensuring only ip address or  maybe even valid domain name is entered. Probably best done with an IF statement that checks for a valid IP and if not produces a pop up saying 'Please enter a valid IP' and breaks out the function
         
-   
+
+        
+        device_ip_address = self.device_ip_address
+        result_of_ping = self.result_of_ping
+
+        device_ip_address[0] = self.ids._IPv4_Target_Device_Layout_.ids.IPv4AddressTextInput.text
+        
+        
+        retry_check = self.ids._Net_Testing_Ping_Layout_.ids.RetryAmountSpinner.text
+
+        if retry_check == 'No Retries':
+            retry_amount = 0
+        else:
+            retry_amount = self.ids._Net_Testing_Ping_Layout_.ids.RetryAmountSpinner.text
+
+        
+        responses, no_responses = multi_ping(device_ip_address, timeout=0.5, retry= retry_amount, ignore_lookup_errors=True)
+
+
+        
+
+        if responses:
+            print("    reponses: %s" % list(responses.keys()))
+            result_of_ping = 'Success'
+        if no_responses:
+            print("    no response received in time, even after retries: %s" %
+                  no_responses)
+            result_of_ping = 'Failed'
+
+
+        if device_ip_address[0] == '':
+            self.ids._Net_Testing_Ping_Layout_.ids.ResultsLabel.text = "Result of ping to '[i]Local Host[/i]' - [b] " + result_of_ping + " [/b]"
+        else:       
+            self.ids._Net_Testing_Ping_Layout_.ids.ResultsLabel.text = "Result of ping to '[i]" + str(device_ip_address[0]) + "[/i]' - [b] " + result_of_ping + " [/b]"
